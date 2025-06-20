9f2d385fd2b96bb38fef143dd07757b346068a8f
Nagandla-Hemalatha <nagandla2001@gmail.com>
2025-06-20 12:20:06 +0530

    terraform modules started

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 
 module "ec2" {
     source = "../terraform-aws-instance"
-    sg_ids = ["sg-0ca6f2de10cc99f46"]
+    sg_ids = var.security_group_ids
     instance_type = var.instance_type
     tags = var.tags
 }
